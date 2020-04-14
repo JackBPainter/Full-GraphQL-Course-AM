316aa93db6bec8c7c91871c093e70258008aa3c1
Jack Painter <littlesympathy@jacks-mbp.home>
2020-04-14 18:12:37 +0100

    Using prisma bindings to log comments and users

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -4,3 +4,11 @@ const prisma = new Prisma({
     typeDefs: 'src/generated/prisma.graphql',
     endpoint: 'http://localhost:4466'
 })
+
+const users = prisma.query.users(null, '{ id name posts { id title body }}').then((data) => {
+    console.log(JSON.stringify(data, undefined, 2))
+}) 
+
+const comments = prisma.query.comments(null, '{ id text author { id name }}').then(data => {
+    console.log(JSON.stringify(data, undefined, 2))
+})
