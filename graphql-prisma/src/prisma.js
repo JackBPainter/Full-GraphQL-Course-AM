582ae6e737062836b5d5247bb6fe4d8f21561c40
Jack Painter <littlesympathy@jacks-mbp.home>
2020-04-14 19:48:16 +0100

    Reduce .then usage by using async await

--- graphql-prisma/src/prisma.js
+++ graphql-prisma/src/prisma.js
@@ -5,40 +5,75 @@ const prisma = new Prisma({
     endpoint: 'http://localhost:4466'
 })
 
-// const users = prisma.query.users(null, '{ id name posts { id title body }}').then((data) => {
-//     console.log(JSON.stringify(data, undefined, 2))
-// }) 
+const createPostForUser = async (authorID, data) => {
+    const post = await prisma.mutation.createPost({
+        data: {
+            ...data, 
+            author: {
+                connect: {
+                    id: authorID
+                }
+            }
+        }
+    }, '{ id }')
 
-// const comments = prisma.query.comments(null, '{ id text author { id name }}').then(data => {
-//     console.log(JSON.stringify(data, undefined, 2))
+    const user = await prisma.query.user({ 
+        where: {
+            id: authorID
+        }
+    }, '{ id name email posts { id title published }}')
+    return user
+}
+
+// createPostForUser('ck8zugylx00ev0860sebvsv2c', {
+//     title: "Good title",
+//     body: "This is a great body",
+//     published: true
+// }).then(user => {
+//     console.log(JSON.stringify(user, undefined, 2))
 // })
 
-// prisma.mutation.createPost({
+const updateUserPost = async (postID, userID, data) => {
+    const updatePost = await prisma.mutation.updatePost({
+        data: {
+            ...data,
+            author: {
+                connect: {
+                    id: userID
+                }
+            }
+        }, 
+        where: {
+            id: postID
+        }
+    }, '{ id }')
+
+    const post = await prisma.query.post({
+        where: {
+            id: postID
+        }
+    }, '{ id title body published author { id name }}')
+
+    return post
+}
+
+updateUserPost('ck907kb3d02le0860k0p156s6', 'ck8zugylx00ev0860sebvsv2c', {
+    title: "Updated post title",
+    body: "Updated body",
+    published: true,
+}).then(data => console.log(JSON.stringify(data, undefined, 2)))
+
+// prisma.mutation.updatePost({
 //     data: {
-//         title: "Post 2",
-//         body: "dead",
-//         published: false,
-//         author: {
-//             connect: {
-//                 id: "ck8zzkybz00qs08602kbs7o79"
-//             }
-//         }
+//         body: "No longer dead",
+//         published: true
+//     }, 
+//     where: {
+//         id: "ck906d86t023s0860cf6wgbfr"
 //     }
 // }, '{ id title body published }').then(data => {
 //     console.log(data)
-//     return prisma.query.users(null, '{ id name posts { id title body }}')
+//     return prisma.query.posts(null, '{ id title body published }')
 // }).then(data => console.log(JSON.stringify(data, undefined, 2)))
 
 
-prisma.mutation.updatePost({
-    data: {
-        body: "No longer dead",
-        published: true
-    }, 
-    where: {
-        id: "ck906d86t023s0860cf6wgbfr"
-    }
-}, '{ id title body published }').then(data => {
-    console.log(data)
-    return prisma.query.posts(null, '{ id title body published }')
-}).then(data => console.log(JSON.stringify(data, undefined, 2)))
