06da2d6af63a7f95803b135d2659336abc601257
Jack Painter <littlesympathy@jacks-mbp.home>
2020-04-17 12:54:24 +0100

    Add authentication to posts and create myPosts query

--- graphql-prisma/src/resolvers/Query.js
+++ graphql-prisma/src/resolvers/Query.js
@@ -17,17 +17,40 @@ const Query = {
 
         return prisma.query.users(opArgs, info)
     },
+    myPosts(parent, { query }, { prisma, request }, info) {
+        const userId = getUserId(request)
+
+        const opArgs = {
+            where: {
+                author: {
+                    id: userId
+                }
+            }
+        }
+
+        if(query) {
+            opArgs.where.OR = [{
+                title_contains: query
+            }, {
+                body_contains: query
+            }]
+        }
+
+        return prisma.query.posts(opArgs, info)
+    },
     posts(parent, args, { prisma }, info) {
-        const opArgs = {}
+        const opArgs = {
+            where: {
+                published: true
+            }
+        }
 
         if(args.query) {
-            opArgs.where = {
-                OR: [{
-                    title_contains: args.query
-                }, {
-                    body_contains: args.query
-                }]
-            }
+            opArgs.where.OR = [{
+                title_contains: args.query
+            }, {
+                body_contains: args.query
+            }]
         }
 
         return prisma.query.posts(opArgs, info)
