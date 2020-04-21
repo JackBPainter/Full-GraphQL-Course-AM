01140f10a624c8b6e034523f4afb9ef495792707
Jack Painter <littlesympathy@jacks-mbp.home>
2020-04-21 11:03:43 +0100

    Add myPost subscription

--- graphql-prisma/src/resolvers/Subscription.js
+++ graphql-prisma/src/resolvers/Subscription.js
@@ -1,3 +1,5 @@
+import getUserId from '../utils/getUserId'
+
 const Subscription = {
   comment: {
     subscribe(parent, { postId }, { prisma }, info) {
@@ -14,21 +16,36 @@ const Subscription = {
         info
       );
     },
-    },
-    post: {
-      subscribe(parent, args, { prisma }, info) {
-        return prisma.subscription.post(
-          {
-            where: {
-              node: {
-                published: true,
-              },
+  },
+  post: {
+    subscribe(parent, args, { prisma }, info) {
+      return prisma.subscription.post(
+        {
+          where: {
+            node: {
+              published: true,
             },
           },
-          info
-        );
-      },
+        },
+        info
+      );
     },
+  },
+  myPost: {
+    subscribe(parent, args, { prisma, request }, info) {
+      const userId = getUserId(request)
+
+      return prisma.subscription.post({
+        where: {
+          node: {
+            author: {
+              id: userId
+            }
+          }
+        }
+      }, info)
+    }
+  }
 };
 
 export default Subscription;
