2f98f61b9ef8d064253119587cf41d291e866d4b
Jack Painter <littlesympathy@jacks-mbp.home>
2020-04-21 12:29:47 +0100

    Add first and skip arguments to users and posts queries

--- graphql-prisma/src/resolvers/Query.js
+++ graphql-prisma/src/resolvers/Query.js
@@ -2,7 +2,10 @@ import getUserId from "../utils/getUserId"
 
 const Query = {
     users(parent, args, { prisma }, info) {
-        const opArgs = {}
+        const opArgs = {
+            first: args.first,
+            skip: args.skip
+        }
 
         if(args.query) {
             opArgs.where = {
@@ -39,7 +42,9 @@ const Query = {
         const opArgs = {
             where: {
                 published: true
-            }
+            },
+            first: args.first,
+            skip: args.skip
         }
 
         if(args.query) {
