f51030319126b7f0afff5a9648244b2ef8e9b145
Jack Painter <littlesympathy@jacks-mbp.home>
2020-04-22 16:44:41 +0100

    Create additional test cases

--- graphql-prisma/tests/user.test.js
+++ graphql-prisma/tests/user.test.js
@@ -1,7 +1,32 @@
-test('This is my first test case', () => {
-    1 + 2
+import { getFirstName, isValidPassword } from '../src/utils/user.js'
+
+test('Should return first name when given full name', () => {
+    const firstName = getFirstName('Andrew Mead')
+
+    expect(firstName).toBe('Andrew')
 })
 
-test('This is my second test case', () => {
-    5 + 10
+test('Should return first name when given first name', () => {
+    const firstName = getFirstName('Jen')
+
+    expect(firstName).toBe('Jen')
+})
+
+test('Should reject password shorter than 8 characters', () => {
+    const isValid = isValidPassword('Short')
+
+    expect(isValid).toBe(false)
+})
+
+test('Should reject password that contains the word password', () => {
+    const isValid = isValidPassword('Password')
+
+    expect(isValid).toBe(false)
+})
+
+test('Should correctly validate a valid password', () => {
+    const password = 'ThisWillWork'
+    const isValid = isValidPassword(password)
+
+    expect(isValid).toBe(true)
 })
