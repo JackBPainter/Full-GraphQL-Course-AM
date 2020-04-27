620ae6eabbcc748442aa9dd76fc3fe172dd65970
Jack Painter <littlesympathy@jacks-mbp.home>
2020-04-27 12:32:46 +0100

    Configure jest to start graphql server (Awaiting fix on babel-register not found bug)

---
+++ graphql-prisma/tests/jest/globalSetup.js
@@ -0,0 +1,10 @@
+require('@babel-register')({
+    ignore: [],
+})
+
+require('@babel/polyfill/noConflict')
+const server = require('../../src/server').default
+
+module.exports = async () => {
+    global.httpServer = await server.start({ port: 4000 })
+}
